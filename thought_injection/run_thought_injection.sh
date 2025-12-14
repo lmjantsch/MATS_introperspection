e9651b9d3f94dc4f9bb9232f624295b1f0e95a18
lmjantsch <lassejantsch@web.de>
2025-12-15 00:55:15 +0900

    thought injection

---
+++ thought_injection/run_thought_injection.sh
@@ -0,0 +1,34 @@
+# model_id=meta-llama/Llama-3.1-8B-Instruct
+# for layer in 15 20 23; do
+#     for magnitude in 2.0 4.0; do
+#         python3 thought_injection.py $model_id $layer $magnitude
+#     done
+# done
+
+# model_id=Qwen/Qwen2.5-7B-Instruct
+# for layer in 13 18 22; do
+#     for magnitude in 2.0 4.0; do
+#         python3 thought_injection.py $model_id $layer $magnitude
+#     done
+# done
+
+# model_id=google/gemma-2-9b-it
+# for layer in 20 27 34; do
+#     for magnitude in 2.0 4.0; do
+#         python3 thought_injection.py $model_id $layer $magnitude
+#     done
+# done
+
+# model_id=tiiuae/Falcon3-7B-Instruct
+# for layer in 13 18 22; do
+#     for magnitude in 2.0 4.0; do
+#         python3 thought_injection.py $model_id $layer $magnitude
+#     done
+# done
+
+model_id=meta-llama/Llama-3.3-70B-Instruct
+for layer in 39 52 66; do
+    for magnitude in 2.0 4.0; do
+        python3 thought_injection.py $model_id $layer $magnitude
+    done
+done
